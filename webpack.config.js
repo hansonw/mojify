08a2ad1a1758828aa639f9ea124af9668de12280
hansonw <hansonw@fb.com>
2015-12-27 01:25:08 -0500

    Live reload support

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+var LiveReloadPlugin = require('webpack-livereload-plugin');
 
 module.exports = {
   cache: true,
@@ -13,12 +14,13 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/ , loader: 'babel' },
-      // { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
       // font-awesome
       // { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
       // { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' }
     ]
   },
   plugins: [
+    new LiveReloadPlugin()
   ]
 };
