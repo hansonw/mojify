e41ec220337d0a799e2feb2c9bbf316a679cff07
hansonw <hansonw@fb.com>
2015-12-27 01:52:38 -0500

    Properly differentiate between dev/prod builds.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,34 @@
 var webpack = require('webpack');
 var LiveReloadPlugin = require('webpack-livereload-plugin');
 
+var BUILD_DEV = !!process.env.BUILD_DEV;
+var definePlugin = new webpack.DefinePlugin({
+  __DEV__: JSON.stringify(JSON.parse(BUILD_DEV)),
+  "process.env": {
+    NODE_ENV: BUILD_DEV ? undefined : JSON.stringify("production")
+  }
+});
+
+var plugins = [
+  definePlugin,
+];
+
+if (BUILD_DEV) {
+  plugins.push(new LiveReloadPlugin());
+} else {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
+  }));
+}
+
 module.exports = {
   cache: true,
   entry: {
     popup: './lib/popup.js',
     content: './lib/content.js',
+    background: './lib/background.js',
   },
   output: {
     path: 'build',
@@ -20,7 +43,5 @@ module.exports = {
       // { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' }
     ]
   },
-  plugins: [
-    new LiveReloadPlugin()
-  ]
+  plugins: plugins
 };
