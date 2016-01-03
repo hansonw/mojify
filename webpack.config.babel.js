5a6d239c9b2a596d904a515d41bb941dc57d5052
hanson.wang <hanson.wng@gmail.com>
2016-01-02 16:17:00 -0800

    Clean up requires, babel, and eslint.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import webpack from 'webpack';
 import LiveReloadPlugin from 'webpack-livereload-plugin';
 
@@ -35,8 +36,17 @@ export default {
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel' },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, 'lib'),
+        ],
+      },
+      {
+        loader: 'style-loader!css-loader',
+        test: /\.css$/,
+      },
       // font-awesome
       // { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
       // { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
