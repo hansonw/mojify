5d432a4bd37fee33a3e0b40ed6ae170df92fb4f0
hanson.wang <hanson.wng@gmail.com>
2016-05-14 14:27:21 -0700

    Update eslint & react

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -49,7 +49,10 @@ export default {
         test: /\.css$/,
       },
       // font-awesome
-      // { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
+      // {
+      //   test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      //   loader: 'url-loader?limit=10000&minetype=application/font-woff',
+      // },
       // { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
     ],
   },
